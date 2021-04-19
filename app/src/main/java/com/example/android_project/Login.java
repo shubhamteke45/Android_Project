5fbff121c8767d6387b098091e416bbc499d42cb
Sanket Kale <55947684+Sank-Infinity@users.noreply.github.com>
2021-04-20 00:25:40 +0530

    firebase connection by sanket

--- app/src/main/java/com/example/android_project/Login.java
+++ app/src/main/java/com/example/android_project/Login.java
@@ -1,17 +1,112 @@
 package com.example.android_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
 
+
+    Button loginBtn;
+    EditText name_login, password_login;
+    TextView register_link, forget_link;
+
+    String name, password;
+
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Extract id's
+        loginBtn = findViewById(R.id.login_btn);
+
+        name_login = findViewById(R.id.name_login);
+        password_login = findViewById(R.id.password_login);
+
+        register_link = findViewById(R.id.register_link);
+        forget_link = findViewById(R.id.forgetPasswordLogin);
+
+        //fAuth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name = name_login.getText().toString();
+                password = password_login.getText().toString();
+
+                if(name.isEmpty()){
+                    name_login.setError("Please check Email");
+                    return;
+                }
+                if(password.isEmpty()){
+                    password_login.setError("Please check your password");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(name, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(getApplicationContext(), ConsumerModernDashboard.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        register_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
+
+        forget_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // TO check whether user is logged in or not
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+            DocumentReference documentReference = FirebaseFirestore.getInstance().collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    startActivity(new Intent(getApplicationContext(), ConsumerModernDashboard.class));
+                }
+            });
+
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/android_project/Register.java
+++ app/src/main/java/com/example/android_project/Register.java
@@ -1,14 +1,122 @@
 package com.example.android_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+
+    public static final String TAG = "TAG";
+    Button signUp;
+    EditText nameRegister, addressRegister, phoneRegister, emailRegister, passwordRegister ;
+    String name, address, phone, email, password, userId;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        //extract ids
+        signUp = findViewById(R.id.registerBtn);
+
+        nameRegister = findViewById(R.id.nameRegister);
+        addressRegister = findViewById(R.id.addressRegister);
+        phoneRegister = findViewById(R.id.phoneRegister);
+        emailRegister = findViewById(R.id.EmailRegister);
+        passwordRegister = findViewById(R.id.passwordRegister);
+
+        //button action listener
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name = nameRegister.getText().toString();
+                address = addressRegister.getText().toString();
+                phone = phoneRegister.getText().toString();
+                email = emailRegister.getText().toString();
+                password = passwordRegister.getText().toString();
+
+                if(name.isEmpty()){
+                    nameRegister.setError("Please Enter Full Name");
+                    return;
+                }
+                if(address.isEmpty()){
+                    addressRegister.setError("Please Enter Address");
+                    return;
+                }
+                if(phone.isEmpty()){
+                    phoneRegister.setError("Please Enter Phone Number");
+                    return;
+                }
+                if(email.isEmpty()){
+                    emailRegister.setError("Please Enter Email");
+                    return;
+                }
+                if(password.isEmpty()){
+                    passwordRegister.setError("Please Enter Password");
+                    return;
+                }
+                if(password.length() < 6){
+                    passwordRegister.setError("Enter password Length more than 6");
+                    return;
+                }
+
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "User Created", Toast.LENGTH_LONG).show();
+                            //Fetching User Id
+                            userId = fAuth.getCurrentUser().getUid();
+                            //Creating new Document at firestore
+                            DocumentReference documentReference = fStore.collection("Users").document(userId);
+                            //Create Hash map
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fname", name);
+                            user.put("faddress", address);
+                            user.put("fphone", phone);
+                            user.put("femail", email);
+                            user.put("fpassword", password);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: User Profile is created for "+userId);
+                                }
+                            });
+
+                            startActivity(new Intent(getApplicationContext(), Login.class));
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Error!" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
